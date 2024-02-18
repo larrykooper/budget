4fe8976b73af407a5023fc7eb07b252bab7107ef
larrykooper <larry.kooper@gmail.com>
2024-02-18 12:52:28 -0500

    added transaction types

--- src/translation/transaction_type_autotranslation.py
+++ src/translation/transaction_type_autotranslation.py
@@ -1,5 +1,22 @@
 class TransactionTypeAutotranslation:
     # from: to
     trans_type_autotranslation = {
+        "Adjustment": "adjustment",
+        "Fee": "debit",
+        "Payment": "transfer",
+        "Payment/Credit": "transfer",
+        "Purchase": "debit",
+        "Return": "credit",
         "Sale": "debit"
+    }
+
+    bank_trans_detail_to_trans_type = {
+        "ACH_CREDIT": "credit",
+        "ACH_DEBIT": "debit",
+        "ATM": "debit",
+        "CHECK_PAID": "debit",
+        "DEBIT_CARD": "debit",
+        "FEE_TRANSACTION": "debit",
+        "LOAN_PMT": "transfer",
+        "MISC_DEBIT": "debit"
     }
